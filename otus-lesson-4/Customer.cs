36f14b49fd6b21d0046682029216fa5b7b4d4aa6
o.rovenskiy@dcoa.ru <o.rovenskiy@dcoa.ru>
2023-05-19 22:46:03 +0300

    Scaffold-DbContext execution

---
+++ otus-lesson-4/Customer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace otus_lesson_4;
+
+public partial class Customer
+{
+    public int Id { get; set; }
+
+    public string? Firstname { get; set; }
+
+    public string? Lastname { get; set; }
+
+    public string? Email { get; set; }
+
+    public int? Age { get; set; }
+
+    public virtual ICollection<Order> Orders { get; set; } = new List<Order>();
+}
---
+++ otus-lesson-4/Item.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace otus_lesson_4;
+
+public partial class Item
+{
+    public int Id { get; set; }
+
+    public string? Name { get; set; }
+
+    public string? Type { get; set; }
+
+    public string? Description { get; set; }
+
+    public decimal? Price { get; set; }
+
+    public virtual ICollection<Order> Orders { get; set; } = new List<Order>();
+}
