7311b3d0ec6c4022c7c13181c6fd8c99968c639a
o.rovenskiy@dcoa.ru <o.rovenskiy@dcoa.ru>
2023-05-20 15:53:30 +0300

    task 1-3

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,6 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using ConsoleApp1;
+
 Console.WriteLine("Hello, World!");
 
 
+using (Hwlesson4Context db = new Hwlesson4Context())
+{
 
-Console.ReadKey();
+
+
+var customers = db.Customers.ToList();
+    Console.WriteLine("Customers list:");
+
+    
+    
+    foreach (Customer c in customers)
+    {
+        Console.WriteLine($"{c.Id}. {c.Firstname}, {c.Lastname}, {c.Email} - {c.Age}");
+    }
+
+var items = db.Items.ToList();
+    Console.WriteLine("Items list:");
+
+    foreach (var item in items)
+    {
+        Console.WriteLine($"{item.Id}. {item.Name}, {item.Type}, {item.Description}, {item.Price}");
+    }
+
+var orders = db.Orders.ToList();
+    Console.WriteLine("Orders:");
+
+    foreach (var order in orders)
+    {
+        Console.WriteLine($"{order.Id}. {order.Customerid}, {order.Itemid}, {order.Quantity}");
+        Console.WriteLine($"{order.Id}. {order.Customer?.Lastname}, {order.Item?.Name}, {order.Quantity}");
+    }
+
+
+
+}
+
+
+    Console.ReadKey();
