2cb9004f55e20e8719eb2b284e74924f4cf24c49
o.rovenskiy@dcoa.ru <o.rovenskiy@dcoa.ru>
2023-05-20 17:35:30 +0300

    final

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,4 +1,6 @@
 ﻿using ConsoleApp1;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
 
 Console.WriteLine("Hello, World!");
 
@@ -35,6 +37,40 @@ var orders = db.Orders.ToList();
         Console.WriteLine($"{order.Id}. {order.Customer?.Lastname}, {order.Item?.Name}, {order.Quantity}");
     }
 
+    // 4. Добавить в программу возможность добавления в таблицу на выбор.
+
+    Console.WriteLine("Будете добавлять клиентов? Если да, то наберите yes");
+string? choise = Console.ReadLine();
+
+    if (choise == "yes")
+    {
+        Console.WriteLine("Введите Имя");
+        var FirstName = Console.ReadLine();
+
+        Console.WriteLine("Введите Фамилию");
+        var LastName = Console.ReadLine();
+
+        Console.WriteLine("Введите e-mail");
+        var Email = Console.ReadLine();
+
+        Console.WriteLine("Введите возраст");
+        int Age;
+        bool parce = int.TryParse(Console.ReadLine(), out Age);
+
+
+        var ListId = from cust in db.Customers
+                     orderby cust.Id
+                     select cust.Id;
+        var LastId = ListId.Last();
+
+
+        Customer NewCustomer = new Customer { Id = LastId + 1, Firstname = FirstName, Lastname = LastName, Email = Email, Age = Age };
+
+        db.Customers.Add(NewCustomer);        
+        db.SaveChanges();
+        Console.WriteLine("Запись в БД успешно добавлено");   
+
+    }
 
 
 }
